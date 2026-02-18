58fa3897a7821a6a37b0bf3c93a975bf850f852b
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-02-18 15:06:42 +0800

    feat(user): create user model and repository

---
+++ src/module/user/userModel.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+export interface UserInterface{
+    _id: mongoose.Types.ObjectId;
+    username: string;
+    email: string;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const userShema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'username wajib diisi'],
+        unique: [true, 'username harus unik'],
+    },
+    password: {
+        type: String,
+        required: [true, 'password harus diisi']
+    },
+    email: {
+        type: String,
+        required: [true, 'email wajib diisi'], 
+        unique: [true, "email sudah digunakan"]
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        required: true,
+        default: 'user'
+    }
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User', userShema);
+
+export default User;
---
+++ src/module/user/userRepository.ts
@@ -0,0 +1,10 @@
+import User, { UserInterface } from "./userModel";
+
+export const createUser = async (
+    data: {username: string, password: string, email: string}): Promise<UserInterface> => {
+    return await User.create(data);
+}
+
+export const getUserByUsername = async (username: string) => {
+    return await User.findOne({username});
+}
