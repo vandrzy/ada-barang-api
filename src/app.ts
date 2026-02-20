cb0b9afca56a66ed09f9fcf92ab5f5b5f6e5d03a
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-02-21 01:44:10 +0800

    chore: implement cokkie-parser

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db';
 import globalErrorHandler from './util/globalErrorHandler';
 import authRoute from './module/auth/authRoute';
+import cokkieParser from 'cookie-parser';
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(cokkieParser());
 app.use('/api/auth', authRoute);
 
 
