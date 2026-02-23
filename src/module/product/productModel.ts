b4fabab774c0fef1de1e1308e42513c63188cd7e
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-02-23 15:13:09 +0800

    feat(product): create product model

--- src/module/product/productModel.ts
+++ src/module/product/productModel.ts
@@ -1,2 +1,43 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
+export interface ProductInterface extends Document{
+    name: string;
+    description?: string;
+    shortCode: string;
+    imagePublicId: string;
+    imagePublicUrl: string;
+    categories: mongoose.Types.ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    shortCode: {
+        type: String,
+        required: true
+    },
+    imagePublicId: {
+        type: String,
+        required: true
+    },
+    imagePublicUrl: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
+        default: []
+    }
+},{
+    timestamps: true
+});
+
+const Product = mongoose.model<ProductInterface>('Product', productSchema);
+export default Product;
