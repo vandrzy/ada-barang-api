c626b7c8895d94f28d9896251bda5f90e53af276
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-02-25 14:49:29 +0800

    feat (util & config): make cloudinary utility

---
+++ src/util/cloudinaryUtil.ts
@@ -0,0 +1,34 @@
+import { DeleteApiResponse, UploadApiErrorResponse, UploadApiResponse } from "cloudinary";
+import cloudinary from "../config/cloudinary";
+import AppError from "./appError";
+
+export const uploadToCloudinary = (buffer: Buffer, folder: string): Promise<UploadApiResponse> => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream(
+            {folder},
+            (error: UploadApiErrorResponse | undefined, result: UploadApiResponse| undefined) => {
+                if (error) {
+                    return reject(error);
+                }
+                if (!result) {
+                    return reject(new AppError('Upload gagal', 400));
+                }
+                resolve(result);
+            }
+        ).end(buffer);
+    });
+};
+
+export const destroyFromCloudinary = (publicId: string): Promise<DeleteApiResponse> => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.destroy(
+            publicId,
+            (error, result) => {
+                if (error){
+                    return reject(error);
+                }
+                resolve(result);
+            }
+        );
+    });
+};
